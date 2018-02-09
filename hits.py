3ab1fb5c3134a86cc2186d2b3b63b39d42a70d45
Martin Boyanov <mboyanov@gmail.com>
2018-02-09 23:13:17 +0200

    Initial commit  of HITS algorithm - WIP

---
+++ hits.py
@@ -0,0 +1,91 @@
+import collections
+import operator
+from time import time
+from multiprocessing import Pool
+import os
+import math
+import json
+
+
+
+def main(fp):
+    start = time()
+    seen = set()
+    outnodes = collections.defaultdict(lambda: [])
+    innodes = collections.defaultdict(lambda: [])
+    usernames ={}
+    likes=  {}
+    with open(fp) as lines:
+        for line in lines:
+            stuff = line.split("\t")
+            user_from = stuff[0]
+            user_name = stuff[1].strip()
+            likes[user_from] = int(stuff[2].strip())
+            usernames[user_from] = user_name
+            users_to = set()
+            if len(stuff)>3:
+                users_to = set([x.strip() for x in stuff[3].split(",") if x.strip() !=''])
+            outnodes[user_from] = users_to
+            seen.add(user_from)
+            for user in users_to:
+                seen.add(user)
+                innodes[user].append(user_from)
+    oldhubs = collections.defaultdict(lambda: (len(seen) ** -0.5))
+    oldauthorities = collections.defaultdict(lambda: (len(seen) ** -0.5))
+
+    for i in range(150):
+        #print("Iteration %i" % i)
+        sumhubs = 0
+        sumauth = 0
+        newhubs = collections.defaultdict(lambda: 0)
+        newauthorities = collections.defaultdict(lambda: 0)
+        for vertex in seen:
+            for auth in outnodes[vertex]:
+                newhubs[vertex] += oldauthorities[auth]
+
+            sumhubs += newhubs[vertex] ** 2
+        for vertex in seen:
+            for hub in innodes[vertex]:
+                newauthorities[vertex] += oldhubs[hub]
+            newauthorities[vertex] += math.sqrt(likes.get(vertex, 0))
+            sumauth += newauthorities[vertex] ** 2
+        sumhubs = sumhubs ** 0.5
+        sumauth = sumauth ** 0.5
+        #print('sumhub,sumauth', sumhubs, sumauth)
+        newhubs = {t: newhubs[t] / sumhubs for t in newhubs}
+        newauthorities = {t: newauthorities[t] / sumauth for t in newauthorities}
+        delta = sum([abs(newhubs[t] - oldhubs[t]) for t in newhubs])
+        print("Delta", delta)
+        #plot(oldhubs, oldauthorities, i, len(seen) ** -0.5)
+        oldhubs = newhubs
+        oldauthorities = newauthorities
+
+    auth = sorted(oldauthorities.items(), key=operator.itemgetter(1))
+    hubs = sorted(oldhubs.items(), key=operator.itemgetter(1))
+
+    with open('%s.hubs.txt' % fp, 'w') as out:
+        for u_id, score in hubs:
+            out.write("\t".join([str(round(score, 8)),
+                                 str(oldauthorities[u_id]),
+                                 str(u_id),
+                                 usernames[u_id.strip()] if u_id.strip() in usernames else 'unknown',
+                                 'Responded to %i users' % len(outnodes[u_id]),
+                                 'Got responses from %i users' % len(innodes[u_id])]) + "\n")
+
+    with open('%s.auth.txt' % fp, 'w') as out:
+        for u_id, score in auth:
+            out.write("\t".join([str(round(score, 8)),
+                                 str(oldhubs[u_id]),
+                                 str(u_id),
+                                 usernames[u_id.strip()] if u_id.strip() in usernames else 'unknown',
+                                 'Responded to %i users' % len(outnodes[u_id]),
+                                 'Got responses from %i users' % len(innodes[u_id]),
+                                "Received %.2f likes " % likes.get(u_id, 0)]) + "\n")
+    #influential_posts = getUsersPosts(open(fp.replace(".graph", "")), [x[0] for x in auth[-10:]])
+    #with open('%s.auth.top10' % fp, 'w') as out:
+    #    for p in influential_posts:
+    #        out.write(json.dumps(p) + "\n")
+    print(fp + " Complete")
+    return time() - start
+
+
